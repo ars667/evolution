6faff793732962609c3f66fba5d367553f0b2282
Ars Shc <shchedrin.arsenij@gmail.com>
2022-01-25 22:10:49 +0300

    3

--- game.py
+++ game.py
@@ -2,10 +2,13 @@ import pygame
 import random
 import math
 
+# основные переменные
 WIDTH = 800
 HEIGHT = 480
-FPS = 10
-number_of_food = 10
+FPS = 60
+number_of_food = 10 #*2
+number_of_men = 0
+speed = 2
 
 # Задаем цвета
 WHITE = (255, 255, 255)
@@ -18,7 +21,7 @@ GREY = (185, 185, 185)
 
 # Создаем игру и окно
 
-class Mob(pygame.sprite.Sprite):
+class food(pygame.sprite.Sprite):  # описание пищи
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10, 10))
@@ -36,7 +39,7 @@ class Mob(pygame.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 
-class SMH(pygame.sprite.Sprite):
+class man(pygame.sprite.Sprite):  # описание существа
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 30))
@@ -65,11 +68,11 @@ class SMH(pygame.sprite.Sprite):
         elif self.rect.bottom > m1.rect.bottom:
             dy = -1
 
-        self.rect.x += dx * 10
-        self.rect.y += dy * 10
+        self.rect.x += dx * speed
+        self.rect.y += dy * speed
 
 
-class Player1(pygame.sprite.Sprite):
+class Player1(pygame.sprite.Sprite):  # описание существа
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 30))
@@ -97,8 +100,8 @@ class Player1(pygame.sprite.Sprite):
         elif self.rect.bottom > m.rect.bottom:
             dy = -1
 
-        self.rect.x += dx * 10
-        self.rect.y += dy * 10
+        self.rect.x += dx * speed
+        self.rect.y += dy * speed
 
 
 pygame.init()
@@ -108,24 +111,28 @@ pygame.display.set_caption("EVOLUTION")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 players = pygame.sprite.Group()
-mobs = pygame.sprite.Group()
+foods = pygame.sprite.Group()
 player1 = Player1()
-smh = SMH()
+smh = man()
 
 all_sprites.add(player1)
 all_sprites.add(smh)
 players.add(player1)
 players.add(smh)
 for i in range(number_of_food):
-    m = Mob()
-    m1 = Mob()
+    m = food()
+    m1 = food()
     all_sprites.add(m)
-    mobs.add(m)
+    foods.add(m)
     all_sprites.add(m1)
-    mobs.add(m1)
+    foods.add(m1)
+for i in range(number_of_men):
+    man1 = man()
+    all_sprites.add(man1)
+    players.add(man1)
+
 # Цикл игры
-SIT = 0
-SIT2 = 0
+SIT = 0  # сытости существ
 SIT3 = 0
 running = True
 while running:
@@ -140,37 +147,31 @@ while running:
 
     # Обновление
     all_sprites.update()
-
+    if len(foods.sprites()) == 1:
+        pygame.quit()
+        print('СЫТОСТЬ СУЩЕСТВА 1 = ', SIT)
+        print('СЫТОСТЬ СУЩЕСТВА 3 = ', SIT3)
     # Проверка, не съело ли существо еду
-    hits1 = pygame.sprite.spritecollide(player1, mobs, False)
+    hits1 = pygame.sprite.spritecollide(player1, foods, False)
     if hits1:
         SIT += 1
-        print('СЫТОСТЬ СУЩЕСТВА 1 = ', SIT)
 
-    hits3 = pygame.sprite.spritecollide(smh, mobs, False)
+    hits3 = pygame.sprite.spritecollide(smh, foods, False)
     if hits3:
         SIT3 += 1
-        print('СЫТОСТЬ СУЩЕСТВА 3 = ', SIT3)
-    hits = pygame.sprite.groupcollide(mobs, players, True, False)
+
+    hits = pygame.sprite.groupcollide(foods, players, True, False)
     if hits:
-        best_food = mobs.sprites()[1]
-        for i in range(len(mobs.sprites())):
-            if math.sqrt((mobs.sprites()[i].rect.x - smh.rect.x) ** 2 + (
-                    mobs.sprites()[i].rect.y - smh.rect.y) ** 2) < math.sqrt(
-                (best_food.rect.x - smh.rect.x) ** 2 + (best_food.rect.y - smh.rect.y) ** 2) and mobs.sprites()[
-                i].rect.x != smh.rect.x and mobs.sprites()[i].rect.y != smh.rect.x:
-                best_food = mobs.sprites()[i]
+        best_food = foods.sprites()[0]
+        for i in range(len(foods.sprites())):
+            if math.sqrt((foods.sprites()[i].rect.x - smh.rect.x) ** 2 + (
+                    foods.sprites()[i].rect.y - smh.rect.y) ** 2) < math.sqrt(
+                (best_food.rect.x - smh.rect.x) ** 2 + (best_food.rect.y - smh.rect.y) ** 2) and foods.sprites()[
+                i].rect.x != smh.rect.x and foods.sprites()[i].rect.y != smh.rect.x:
+                best_food = foods.sprites()[i]
         m1 = best_food
-        m = random.choice(mobs.sprites())
-    # for hit in hits:
-    # SIT += 1
-    # print(SIT)
-    #   p1 = player1
-    #  p2 = player2
-    # all_sprites.add(p1)
-    # players.add(p1)
-    # all_sprites.add(p2)
-    # players.add(p2)
+        m = random.choice(foods.sprites())
+
     # Рендеринг
 
     screen.fill(GREY)
